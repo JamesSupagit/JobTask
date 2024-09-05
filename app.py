73c3a39f06dce1dd3f4b8086cc8407fea356a453
SR9 <supagit.ruarn@gmail.com>
2024-09-05 13:11:41 +0700

    update

--- app.py
+++ app.py
@@ -186,7 +186,7 @@ def job():
 # Schedule the job at 17:30 GMT+7 every day
 def schedule_job():
     tz = pytz.timezone('Asia/Bangkok')
-    scheduler.add_job(job, 'cron', hour=17, minute=30, timezone=tz)
+    scheduler.add_job(job, 'cron', hour=13, minute=15, timezone=tz)
 
 # Start the scheduler
 scheduler.start()
